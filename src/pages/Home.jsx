bf84b943d1c659073dc85efe590dd4b43cc47d4b
Alexjandro Mendoza <alexjandromendoza@macbook-pro.mynetworksettings.com>
2022-04-23 01:08:39 -0400

    the home page can defintely use some work but its 1AM, must recharge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Note from './docs/Note.pdf'
 
-function Home() {
+
+
+export const Home = () => {
   return (
-    <div>Home</div>
+    <> 
+   
+    <div>
+      It has been 5 years so far and everyday, 
+      <br />
+      whether dark & gloomy or shiny & delightful
+      <br/> I'm grateful to spend w you
+      <br/>
+      <br/>
+      I Love You  
+      <br/>
+      <br/>
+      <small>Find out my first thoughts
+        <br/>
+        </small>
+
+      <button class="btn btn-outline btn-primary">
+        
+      <a href={Note} download="Note.pdf"> Note </a>
+
+      </button>
+      
+
+
+    </div>
+
+    </>
   )
 }
 
